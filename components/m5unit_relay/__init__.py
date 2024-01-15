bf3492b71bef76a023bc00cb95e69d2706ed2d20
Michael Koval <michael@koval.fyi>
2024-01-15 23:00:38 +0000

    Fix duplicate constructor

--- components/m5unit_relay/__init__.py
+++ components/m5unit_relay/__init__.py
@@ -46,5 +46,7 @@ async def to_code(config):
     await cg.register_component(var, config)
     await i2c.register_i2c_device(var, config)
 
+    """
     for channel, switch_config in enumerate(config[CONF_SWITCHES]):
         await channel_to_code(parent=var, channel=channel, config=switch_config)
+    """
