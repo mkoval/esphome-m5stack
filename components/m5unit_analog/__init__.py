be043b6453c142f97bbedda1d39338214542e1cd
Michael Koval <michael@koval.fyi>
2024-01-15 19:46:18 +0000

    Switch from yield to await

--- components/m5unit_analog/__init__.py
+++ components/m5unit_analog/__init__.py
@@ -50,10 +50,10 @@ CONFIG_SCHEMA = (
     })
 )
 
-def to_code(config):
+async def to_code(config):
     var = cg.new_Pvariable(config[CONF_ID])
-    yield cg.register_component(var, config)
-    yield i2c.register_i2c_device(var, config)
+    await cg.register_component(var, config)
+    await i2c.register_i2c_device(var, config)
 
     for i, sensor_config in enumerate(config[CONF_SENSORS]):
         sensor_obj = cg.Pvariable(conf[CONF_ID], var.get_sensor(i))
